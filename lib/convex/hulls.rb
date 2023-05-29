1e88092c23ac1808b5add02ed69596794acdf852
lfybbk10 <63192458+lfybbk10@users.noreply.github.com>
2023-05-29 19:30:46 +0300

    init commit

---
+++ lib/convex/hulls.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "hulls/version"
+
+module Convex
+  module Hulls
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/convex/hulls/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Convex
+  module Hulls
+    VERSION = "0.1.0"
+  end
+end
