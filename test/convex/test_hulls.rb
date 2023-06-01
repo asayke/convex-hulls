1c14a3ec481b2f5971d59d8dedbf0ffd26bedd35
oleja <noilzep@gmail.com>
2023-06-01 22:55:53 +0300

    Added tests for jarvis algorithm
    Co-Authored-By: lfybbk10 <63192458+lfybbk10@users.noreply.github.com>

--- test/convex/test_hulls.rb
+++ test/convex/test_hulls.rb
@@ -1,13 +1,43 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require "test/unit"
+require_relative '..\..\lib\convex\hulls.rb'
 
-class Convex::TestHulls < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Convex::Hulls::VERSION
+class TestHulls < Test::Unit::TestCase
+  def setup
+    @algorithms = Convex::Hulls::Algorithms.new
   end
 
-  def test_it_does_something_useful
-    assert false
+  test "test_jarvis_convex_hull" do
+    points = [
+      [0, 3],
+      [2, 2],
+      [1, 1],
+      [2, 1],
+      [3, 0],
+      [0, 0],
+      [3, 3]
+    ]
+
+    expected_hull = [
+      [0, 3],
+      [0, 0],
+      [3, 0],
+      [3, 3],
+    ]
+
+    assert_equal expected_hull, @algorithms.jarvis(points)
   end
+
+  test "test_jarvis_convex_hull_empty" do
+    points = [
+      [0, 0],
+      [1, 1],
+    ]
+
+    expected_hull = []
+
+    assert_equal expected_hull, @algorithms.jarvis(points)
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,4 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "convex/hulls"
 
-require "minitest/autorun"
+require "test-unit"
